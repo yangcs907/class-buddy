91a545e445b81d3db3864cf266d350a66ac3968f
yangcs907 <>
2019-05-14 15:23:11 -0400

    updates

--- config/keys_production.js
+++ config/keys_production.js
@@ -1,7 +1,7 @@
 // Keys for env on heroku
 module.exports = {
-  mongoURI: process.env.mongoURI,
-  secretOrKey: process.env.secretOrKey,
-  apiKey: process.env.apiKey,
-  classID: process.env.classID
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  apiKey: process.env.API_KEY,
+  classID: process.env.CLASS_ID
 };
